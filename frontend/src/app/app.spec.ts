0488324e5876538560da6ec0d870887e602bca83
Henri <judo.henri@gmx.de>
2026-02-05 18:45:54 +0100

    feat: add router and activated route providers to app testing module

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,10 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App]
+      imports: [App],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { paramMap: convertToParamMap({}) },
+            queryParamMap: of(convertToParamMap({})),
+            paramMap: of(convertToParamMap({}))
+          }
+        }
+      ]
     }).compileComponents();
   });
 
